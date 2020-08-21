4ff14981938d5d38a74ad1d4ab2084043d5f0650
Steve Baker <_steve_@outlook.com>
2020-08-21 10:17:42 +0200

    Add PostgreSQL to SQL (#219)

--- core/Parsing.Documents.fs
+++ core/Parsing.Documents.fs
@@ -234,7 +234,7 @@ let mutable languages = [
         java
     lang "Shell script" "shellscript" ".sh"
         configFile
-    lang "SQL" "" ".sql"
+    lang "SQL" "postgres" ".pgsql|.psql|.sql"
         ( sourceCode [ line "--"; cBlock ] )
     lang "Swift" "" ".swift"
         java
